9aec025289eb68acf7a3bf715e118a00a8a072bd
Abhishek Mahato <57245064+foreverabhi2002@users.noreply.github.com>
2021-02-19 09:35:09 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', 
+{
+	pagination: 
+	{
+		el: '.swiper-pagination',
+		    dynamicBullets: true,
+	},
+	autoplay: 
+	{
+		delay: 3400,
+		speed: 300,
+	},
+});
