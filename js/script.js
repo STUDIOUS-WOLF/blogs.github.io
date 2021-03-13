0aaac2ead27693b5160d802d216df9afe53af856
Amit Kumar <52822539+STUDIOUS-WOLF@users.noreply.github.com>
2021-03-13 16:43:50 +0530

    added about page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-var swiper = new Swiper('.swiper-container', 
+
+ function myFunction()
+ {
+	 document.getElementById("nav").classList.toggle("show");
+ }
+ window.onclick=function(event)
+ {
+	 if(!event.target.matches('.btn'))
+	 {
+		 var dropdowns = document.getElementsByClassName("navElements");
+		 var i;
+		 for(i=0;i<dropdowns.length;i++)
+		 {
+			 var openDropdown=dropdowns[i];
+			 if(openDropdown.classList.contains('show'))
+			 {
+				 openDropdown.classList.remove('show');
+			 }
+		 }	 
+	}
+ }
+ var swiper = new Swiper('.swiper-container', 
 {
 	pagination: 
 	{
